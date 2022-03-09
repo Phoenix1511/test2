4e25b98286ba0d1d00d922499c21018f7b6f56d8
admin <admin@Bcalab3-m05>
2022-03-09 09:29:15 +0530

    COMMIT1

---
+++ testproject2/src/testproject2/test.java
@@ -0,0 +1,11 @@
+package testproject2;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HEY, Its LUCIFER");
+
+	}
+
+}
